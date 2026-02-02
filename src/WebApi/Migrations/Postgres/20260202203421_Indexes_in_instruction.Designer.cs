b69fd7ac5f1df3fb7b91a630520267138154d445
Vitalii Priadilia <vitalii.priadilia@fores.group>
2026-02-02 21:34:41 +0100

    Add missing migration

--- src/WebApi/Migrations/Postgres/20260202203421_Indexes_in_instruction.Designer.cs
+++ src/WebApi/Migrations/Postgres/20260202203421_Indexes_in_instruction.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WebApi.Infrastructure;
@@ -11,9 +12,11 @@ using WebApi.Infrastructure;
 namespace WebApi.Migrations.Postgres
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260202203421_Indexes_in_instruction")]
+    partial class Indexes_in_instruction
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
