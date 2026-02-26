3a21877a2d66d1cd4bba3513fc45931ba6f91093
Vitalii Priadilia <vitalii.priadilia@fores.group>
2026-02-26 21:48:50 +0100

    feat - update configuration limits

--- src/Agent.WindowsService/Domain/Configuration.cs
+++ src/Agent.WindowsService/Domain/Configuration.cs
@@ -7,13 +7,13 @@ public record Configuration
   public string ServerCertificatedUrl { get; set; } = string.Empty;
   public string ServerNotCertificatedUrl { get; set; } = string.Empty;
   public string Tag { get; set; } = "default";
-  public int AuthenticationExitIntervalSeconds { get; set; } = 5;
-  public int RunningExitIntervalSeconds { get; set; } = 5;
-  public int ExecutionExitIntervalSeconds { get; set; } = 5;
-  public int InstructionsExecutionLimit { get; set; } = 10;
-  public int InstructionResultsSendLimit { get; set; } = 10;
-  public int MetricsSendLimit { get; set; } = 10;
-  public int IterationDelaySeconds { get; set; } = 5;
+  public int AuthenticationExitIntervalSeconds { get; set; } = 10;
+  public int RunningExitIntervalSeconds { get; set; } = 10;
+  public int ExecutionExitIntervalSeconds { get; set; } = 10;
+  public int InstructionsExecutionLimit { get; set; } = 15;
+  public int InstructionResultsSendLimit { get; set; } = 15;
+  public int MetricsSendLimit { get; set; } = 20;
+  public int IterationDelaySeconds { get; set; } = 20;
   public IReadOnlyList<string> AllowedCollectors { get; set; } = ["cpu_usage", "memory_usage", "disk_usage"];
   public IReadOnlyList<string> AllowedInstructions { get; set; } = ["Shell", "Gpo", "Config"];
   public string? EnrollmentToken { get; set; }
